a811559a7fe8f4fc0ce915900eb7125d023f1cea
SangHyune546 <48785671+SangHyune546@users.noreply.github.com>
2022-04-08 19:00:58 +0900

    Update boj_4963.py

--- 22_0409/boj_4963.py
+++ 22_0409/boj_4963.py
@@ -1,46 +1,64 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10000)
 
 input = sys.stdin.readline
 
-def bfs(start_x,start_y):
 
+def bfs(i,j):
+    
     q = deque()
-    q.append([start_x,start_y])
+    q.append((i,j))
 
-    graph[start_x][start_y] = 0
+    graph[i][j] = 0
 
     while q:
 
         x,y = q.popleft()
 
+
         for i in range(len(dx)):
+
             nx = x + dx[i]
             ny = y + dy[i]
 
             if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
-                graph[nx][ny] = 0
                 q.append((nx,ny))
-                
+                graph[nx][ny] = 0
+
+def dfs(x,y):
+
+    graph[x][y] = 0
+
+    for i in range(len(dx)):
+
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
+                dfs(nx,ny)
+
 
 while True:
 
-    count = 0
-
-    dx = [1, -1, 0, 0, 1, -1, 1, -1] #대각선 처리를위해 상 하 좌 우 + 대각상하좌우
-    dy = [0, 0, -1, 1, -1, -1, 1, 1]
-
     w,h = map(int,input().split())
+
+    count = 0
 
     if w == 0 and h == 0:
         break
 
     graph = [list(map(int,input().split())) for _ in range(h)]
 
+    dx = [1,0,-1,0,1,-1,1,-1]
+    dy = [0,1,0,-1,1,1,-1,-1]
+
     for i in range(h):
         for j in range(w):
             if graph[i][j] == 1:
-                bfs(i,j)  
+                dfs(i,j)
+                #bfs(i,j)
                 count += 1
 
-    print(count)  
+    print(count)
+
