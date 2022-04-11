3345d35ad889817d2bca42007e22b4b0b2e5882c
woongsu0614 <woongsu0614@gmail.com>
2022-04-12 00:13:13 +0900

    2417 solve separate 2417-deprecate

---
+++ 002/2417-deprecate.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+#N의 정수 제곱근 구하기
+#0=0, 1=1, 2와 3은 없다
+#4부터는 그 수 / 2 에 대해서만 찾아보면 됨
+start, end = 0, int(N/2)
+while start<=end:
+    mid=(start+end)//2
+    target=mid**2
+    if target>N:
+        end=mid-1
+    elif target>N:
+        start=mid+1
+    else:
+        break
+print(int(start))
--- 002/2417.py
+++ 002/2417.py
@@ -1,9 +1,12 @@
 import sys
 input=sys.stdin.readline
 N=int(input())
-#N의 정수 제곱근 구하기
-#0=0, 1=1, 2와 3은 없다
-#4부터는 그 수 / 2 에 대해서만 찾아보면 됨
-def binary():
-    
-start, end = 
+start=0
+end=N
+while start<=end:
+    mid=(start+end)//2
+    if mid**2<N:
+        start=mid+1
+    else:
+        end=mid-1
+print(start)
