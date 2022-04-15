bba87fd598ae5659ef75e5b1f00d6b37ee67d6d2
woongsu0614 <woongsu0614@gmail.com>
2022-04-15 16:27:47 +0900

    other changes

--- 002/2417-deprecate.py
+++ 002/2417-deprecate.py
@@ -4,14 +4,19 @@ N=int(input())
 #N의 정수 제곱근 구하기
 #0=0, 1=1, 2와 3은 없다
 #4부터는 그 수 / 2 에 대해서만 찾아보면 됨
-start, end = 0, int(N/2)
-while start<=end:
-    mid=(start+end)//2
-    target=mid**2
-    if target>N:
-        end=mid-1
-    elif target>N:
-        start=mid+1
-    else:
-        break
-print(int(start))
+if N == 0:
+    print(0)
+elif N == 1:
+    print(1)
+else:
+    start, end = 2, int(N/2)
+    while start<=end:
+        mid=(start+end)//2
+        target=mid*mid
+        if target>N:
+            end=mid-1
+        elif target>N:
+            start=mid+1
+        else:
+            break
+    print(int(start))
